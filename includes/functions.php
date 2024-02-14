a2d0643905d8a8b0bced2a7f84dd87075883e259
Dennis Enderink <dennis@1815.nl>
2024-02-14 13:22:54 +0100

    feat: added function to stop reporting deprecation notices

--- includes/functions.php
+++ includes/functions.php
@@ -20,3 +20,19 @@ if ( ! function_exists( 'remove_filter_with_instantiated_class' ) ) {
 		);
 	}
 }
+
+if ( ! function_exists( 'wp_debug_ignore_deprecations' ) ) {
+	/**
+	 * Alters error_reporting to ignore deprecations if WP_DEBUG is set. Should be called from
+	 * mu-plugin scope.
+	 *
+	 * @return void
+	 * @phpcs:disable WordPress.PHP.DevelopmentFunctions.prevent_path_disclosure_error_reporting
+	 * @phpcs:disable WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_error_reporting
+	 */
+	function wp_debug_ignore_deprecations(): void {
+		if ( WP_DEBUG ) {
+			error_reporting( error_reporting() & ~E_DEPRECATED );
+		}
+	}
+}
